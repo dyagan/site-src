fb50f39e8a625ff871111803a85a5f39a95aee6a
clancygreen <clancy.c.green@gmail.com>
2019-10-28 14:25:38 -0700

    Fix deploy bug preventing old files from being deleted

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Remove everything from public except for the "git ting"
+# Don't match .git or .gitignore
+find public/ -type 'f' | grep -v ".git" | xargs rm
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
